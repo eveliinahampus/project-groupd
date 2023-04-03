25f7f49e150d9b09b6a09955faf572e9e5e18473
doteins <112283864+doteins@users.noreply.github.com>
2023-04-03 13:44:01 +0300

    Add basic API

---
+++ server/index.ts
@@ -0,0 +1,50 @@
+// Import packages required by the app
+import express, {Express, Request, Response} from "express"
+import cors from "cors"
+import {Pool, QueryResult} from "pg"
+
+// Start new instance of express
+const app: Express = express();
+// Packges used by app
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({extended: false}))
+
+// Port number declaration
+const port: number = 3001;
+
+// Create connection with the database
+const openDB = (): Pool => {
+    const pool: Pool = new Pool ({
+        // user: "postgres",
+        // host: "localhost",
+        // database: "todo",
+        // password: "1234",
+        // port: 5432
+        // user: "root",
+        // host: "dpg-cggp5tu4daddcg550ivg-a.frankfurt-postgres.render.com",
+        // database: "todo_aiux",
+        // password: "Dp7lxWkQlr9Z1PXdojZIyuf8Q1Br3d57",
+        // port: 5432,
+        // ssl: true
+    })
+    return pool
+}
+
+app
+    .get("/",(req: Request, res: Response) => {
+        let pool = openDB()
+
+        pool.query("select * from restaurants", (error,result) => {
+            if (error) {
+                res.status(500).json({error: error.message})
+            }
+            res.status(200).json(result.rows)
+        })
+    })
+
+
+// Port listening
+app.listen(port, () => {
+    console.log(`Server running on http://localhost:${port}`);
+});
