62172e70ca4bf213be99d0ea93122ba3da639b41
doteins <112283864+doteins@users.noreply.github.com>
2023-04-04 13:21:17 +0300

    Add data types

--- server/index.ts
+++ server/index.ts
@@ -16,77 +16,76 @@ const port: number = 3001;
 
 // Set up connection to the database
 const openDB = (): Pool => {
-    const pool: Pool = new Pool ({
-        user: "postgres",
-        host: "localhost",
-        database: "todo",
-        password: "1234",
-        port: 5432
-        // user: "root",
-        // host: "dpg-cggp5tu4daddcg550ivg-a.frankfurt-postgres.render.com",
-        // database: "todo_aiux",
-        // password: "Dp7lxWkQlr9Z1PXdojZIyuf8Q1Br3d57",
-        // port: 5432,
-        // ssl: true
-    })
-    return pool
+  const pool: Pool = new Pool ({
+    user: "postgres",
+    host: "localhost",
+    database: "todo",
+    password: "1234",
+    port: 5432
+    // user: "root",
+    // host: "dpg-cggp5tu4daddcg550ivg-a.frankfurt-postgres.render.com",
+    // database: "todo_aiux",
+    // password: "Dp7lxWkQlr9Z1PXdojZIyuf8Q1Br3d57",
+    // port: 5432,
+    // ssl: true
+  })
+  return pool
 }
 
 // Define routes
 app
-    .get("/",(req: Request, res: Response) => {
-        let pool = openDB()
+  .get("/",(req: Request, res: Response) => {
+    let pool = openDB()
 
-        pool.query("select * from restaurants", (error,result) => {
-            if (error) {
-                res.status(500).json({error: error.message})
-            }
-            res.status(200).json(result.rows)
-        })
+    pool.query("select * from restaurants", (error: Error,result: QueryResult) => {
+        if (error) {
+          res.status(500).json({error: error.message})
+        }
+        res.status(200).json(result.rows)
     })
-    .post("/new", (req: Request, res: Response) => {
-        let pool = openDB()
+  })
+  .post("/new", (req: Request, res: Response) => {
+    let pool = openDB()
 
-        pool.query("insert into restaurants (name, address...description) values ($1, $2, ...) returning *",
-        [req.body.description],
-        (error: Error, result: QueryResult) => {
-            if (error) {
-                res.status(500).json({error: error.message})
-            }
-            res.status(200).json({id: result.rows[0].id})
-        })
+    pool.query("insert into restaurants (name, address...description) values ($1, $2, ...) returning *",
+    [req.body.description],
+    (error: Error, result: QueryResult) => {
+      if (error) {
+        res.status(500).json({error: error.message})
+      }
+      res.status(200).json({id: result.rows[0].id})
     })
-    .delete("/delete/:id",async (req: Request, res: Response) => {
-        let pool = openDB()
+  })
+  .delete("/delete/:id",async (req: Request, res: Response) => {
+    let pool = openDB()
+    let id = parseInt(req.params.id)
 
-        let id = parseInt(req.params.id)
-
-        pool.query("delete from restaurants where id = $1",
-        [id],
-        (error: Error, result: QueryResult) => {
-            if (error) {
-                res.status(500).json({error: error.message})
-            }
-            res.status(200).json({id: id})
-        })
+    pool.query("delete from restaurants where id = $1",
+    [id],
+    (error: Error, result: QueryResult) => {
+      if (error) {
+        res.status(500).json({error: error.message})
+      }
+      res.status(200).json({id: id})
     })
-    .put("/update/name/:id", (req: Request, res: Response) => {
-        let pool = openDB()
+  })
+  .put("/update/name/:id", (req: Request, res: Response) => {
+    let pool = openDB()
 
-        let id = req.params.id
-        let description = req.body.description
+    let id = req.params.id
+    let description = req.body.description
 
-        pool.query("update restaurants set name = $1 where id= $2 returning *",
-        [description,id],
-        (error: Error, result: QueryResult) => {
-            if (error) {
-                res.status(500).json({error: error.message})
-            }
-            res.status(200).json({id: result.rows[0].id})
-        })
+    pool.query("update restaurants set name = $1 where id= $2 returning *",
+    [description,id],
+    (error: Error, result: QueryResult) => {
+      if (error) {
+        res.status(500).json({error: error.message})
+      }
+      res.status(200).json({id: result.rows[0].id})
     })
+  })
 
 // Start the server
 app.listen(port, () => {
-    console.log(`Server running on http://localhost:${port}`);
+  console.log(`Server running on http://localhost:${port}`);
 });
