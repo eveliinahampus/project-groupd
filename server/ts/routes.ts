7dfbb7b1bf340f1a79c801db288a268afff4c69f
doteins <112283864+doteins@users.noreply.github.com>
2023-04-11 12:11:32 +0300

    Update routes

--- server/ts/routes.ts
+++ server/ts/routes.ts
@@ -10,33 +10,33 @@ const router: Router = express.Router();
 // Define routes for images
 router
   .get("/api/images", imgController.getAllImages)
-  .get("/api/images/:id", imgController.getImageById)
   .post("/api/images", imgController.createImage)
+  .get("/api/images/:id", imgController.getImageById)
   .put("/api/images/update/:id", imgController.updateImage)
   .delete("api/images/delete/:id", imgController.deleteImage);
 
 // Define routes for restaurants
 router
   .get("/api/restaurants", resController.getAllRestaurants)
-  .get("/api/restaurant/:id", resController.getRestaurantById)
   .post("/apirestaurants", resController.createRestaurant)
+  .get("/api/restaurant/:id", resController.getRestaurantById)
   .put("/api/restaurants/update/name/:id", resController.updateRestaurant)
   .delete("/api/restaurants/delete/:id", resController.deleteRestaurant);
 
 // Define routes for users
 router
   .get("/api/users", userController.getAllUsers)
-  .get("/api/users/:id", userController.getUserById)
   .post("/api/users", userController.createUser)
+  .get("/api/users/:id", userController.getUserById)
   .put("/api/users/update/:id", userController.updateUser)
   .delete("/api/users/delete/:id", userController.deleteUser);
 
 // Define routes for reviews
 router
   .get("/api/reviews", revController.getAllReviews)
+  .post("/api/reviews", revController.createReview)
   .get("/api/reviews/:id", revController.getReviewById)
   .get("/api/reviews/avg/:id", revController.getAverageStars)
-  .post("/api/reviews", revController.createReview)
   .put("/api/reviews/update/:id", revController.updateReview)
   .delete("/api/reviews/delete/:id", revController.deleteReview)
 
