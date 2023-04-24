6a541f5325fccedb26a726a5ded28a23138a8ff7
Yerold Sanabria <112283864+doteins@users.noreply.github.com>
2023-04-24 15:14:18 +0300

    Update db.sql

--- server/db.sql
+++ server/db.sql
@@ -25,7 +25,7 @@ CREATE TABLE users (
   id SERIAL PRIMARY KEY,
   username VARCHAR(100) NOT NULL,
   email VARCHAR(100) NOT NULL,
-  password VARCHAR(100) NOT NULL,
+  password VARCHAR(255) NOT NULL,
   created_at TIMESTAMP NOT NULL DEFAULT NOW()
 );
 
