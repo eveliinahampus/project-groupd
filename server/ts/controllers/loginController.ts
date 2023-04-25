1a9dbda6961c5670e4fc309336f7af8a7bdd7811
doteins <112283864+doteins@users.noreply.github.com>
2023-04-25 19:33:44 +0300

    Update functions and import required modules

--- server/ts/controllers/loginController.ts
+++ server/ts/controllers/loginController.ts
@@ -1,7 +1,7 @@
-import { Request, Response, NextFunction } from "express";
-import authorize from "../middleware/authorize";
+import { Request, Response } from "express";
+import authorize from "../middleware/authenticate";
 
-const loging = (req: Request, res: Response, next: NextFunction) => {
+const login = (req: Request, res: Response) => {
   // This is to handle posted credentials and check if they are valid
 
   // Read username and password from request body
@@ -11,18 +11,23 @@ const loging = (req: Request, res: Response, next: NextFunction) => {
     `Trying to log in with username: ${username} and password: ${password}`
   );
   // Check if the username and password are valid
-  if (username === "mary" && password === "password") {
+  if (username === "admin" && password === "password") {
     // Create a JWT token
     const token = authorize.createToken(username);
     // Save token in a cookie
-    res.cookie("token", token, {
-      httpOnly: true,
-    });
+    res.cookie("token", token, { httpOnly: true, maxAge: 86400000 });
     // Login successful
-    return res.json({ message: "Login successful!", token: token });
+    return res.send({ message: "Logged in successfully" });
   }
   // Login failed
-  return res.status(401).send("Login failed!");
-}
+  return res.status(401).send("Invalid credentials!");
+};
 
-export default { loging }
+// Controller to handle user logout
+const logout = (req: Request, res: Response) => {
+  // Clear the JWT cookie
+  res.clearCookie("token");
+  res.send({ message: "Logged out successfully" });
+};
+
+export default { login, logout };
