d856acc2056a5c782a84b22ab2c0f47653f3d904
doteins <112283864+doteins@users.noreply.github.com>
2023-04-25 15:38:17 +0300

    Update tables

--- server/db.sql
+++ server/db.sql
@@ -6,36 +6,36 @@ CREATE DATABASE restaurant_reviews_db;
 
 CREATE TABLE images (
   id  serial primary key,
-  title varchar(255) NOT NULL,
-  name varchar(255) NOT NULL,
-  user_id INT NOT NULL REFERENCES users(id) ON DELETE CASCADE ON UPDATE CASCADE
+  img_title varchar(255) NOT NULL,
+  img_name varchar(255) NOT NULL,
+  user_id INT NOT NULL REFERENCES users(id) ON DELETE RESTRICT ON UPDATE CASCADE
 );
 
 CREATE TABLE restaurants (
   id SERIAL PRIMARY KEY,
-  name VARCHAR(255) NOT NULL,
-  phone_number VARCHAR(100),
+  restaurant_name VARCHAR(255) NOT NULL,
+  phone_number VARCHAR(255),
   street_address VARCHAR(255) NOT NULL,
-  city VARCHAR(100),
-  zip_code VARCHAR(100),
+  city VARCHAR(255),
+  zip_code VARCHAR(255),
   created_at TIMESTAMP NOT NULL DEFAULT NOW()
 );
 
 CREATE TABLE users (
   id SERIAL PRIMARY KEY,
-  username VARCHAR(100) NOT NULL,
-  email VARCHAR(100) NOT NULL,
+  username VARCHAR(255) NOT NULL,
+  email VARCHAR(255) NOT NULL,
   password VARCHAR(255) NOT NULL,
   created_at TIMESTAMP NOT NULL DEFAULT NOW()
 );
 
 CREATE TABLE reviews (
   id SERIAL PRIMARY KEY,
-  title VARCHAR(100) NOT NULL,
-  body TEXT NOT NULL,
+  review_title VARCHAR(255) NOT NULL,
+  review_body TEXT NOT NULL,
   stars INT NOT NULL CHECK (stars >= 1 AND stars <= 5),
   restaurant_id INT NOT NULL REFERENCES restaurants(id) ON DELETE CASCADE ON UPDATE CASCADE,
-  user_id INT NOT NULL REFERENCES users(id) ON DELETE CASCADE ON UPDATE CASCADE,
+  user_id INT NOT NULL REFERENCES users(id) ON DELETE RESTRICT ON UPDATE CASCADE,
   images_id INT NOT NULL REFERENCES images(id) ON DELETE RESTRICT ON UPDATE RESTRICT,
   created_at TIMESTAMP NOT NULL DEFAULT NOW()
 );
