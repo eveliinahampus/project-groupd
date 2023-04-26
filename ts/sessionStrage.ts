fd17b3bbd59db37594842c21e1b0fba1fe876300
Tomoko Takami <t2tato01@students.oamk.fi>
2023-04-26 21:53:24 +0300

    add typescript about sessionstrage .

--- ts/sessionStrage.ts
+++ ts/sessionStrage.ts
@@ -1,31 +1,37 @@
 // Get the email input element
-const emailInput = document.getElementById("email") as HTMLInputElement;
-// When the submit button is clicked, save the email to sessionStorage
-document.getElementById("submit").addEventListener("click", function(event) {
-  event.preventDefault(); 
-  const email = emailInput.value; 
+//check if there is submitt botton
+if (document.getElementById("submit")) {
+  const emailInput = document.getElementById("email")as HTMLInputElement;
+  // When the submit button is clicked, save the email to sessionStorage
 
-  sessionStorage.setItem("email", email); // keep to the sessionStorage
-  // 他の処理を追加する
+  //
+  document.getElementById("submit").addEventListener("click", function (event) {
+      event.preventDefault();
+      const email = emailInput.value;
+      sessionStorage.setItem("email", email); // keep to the sessionStorage
+      // 他の処理を追加する
+      console.log(sessionStorage.getItem("email"));
+  });
   console.log(sessionStorage.getItem("email"));
 
-});
-console.log(sessionStorage.getItem("email"));
 
+  // Get the username input element
+  const usernameInput = document.getElementById("username")as HTMLInputElement;
+  // When the submit button is clicked, save the email to sessionStorage
+  document.getElementById("submit").addEventListener("click", function (event) {
+      event.preventDefault();
+      const username = usernameInput.value;
+      sessionStorage.setItem("username", username); // keep to the sessionStorage
+      // 他の処理を追加する
+      console.log(sessionStorage.getItem("username"));
+  });
 
-// Get the username input element
-const usernameInput = document.getElementById("username") as HTMLInputElement;
-// When the submit button is clicked, save the email to sessionStorage
-document.getElementById("submit").addEventListener("click", function(event) {
-  event.preventDefault();
-  const username = usernameInput.value; 
+}
+else{
 
-  sessionStorage.setItem("username", username); // keep to the sessionStorage
-  // 他の処理を追加する
-  console.log(sessionStorage.getItem("username"));
-
-});
-console.log(sessionStorage.getItem("username"));
-
-
+console.log("---------",sessionStorage.getItem("username"));
+if (sessionStorage.getItem("username")) {
+  document.getElementById("login-user").innerHTML = sessionStorage.getItem("username");
+}
+}
 
