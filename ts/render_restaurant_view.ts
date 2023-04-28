7ccbeca9800623c74b095145bf46a7a06a2c252e
Tomoko Takami <t2tato01@students.oamk.fi>
2023-04-28 10:26:52 +0300

    showing restaurant name from api

--- ts/render_restaurant_view.ts
+++ ts/render_restaurant_view.ts
@@ -3,7 +3,7 @@ import { Restaurant } from "./class/Restaurant.js";
 
 const BACKEND_ROOT_URL = "http://localhost:3001/api/restaurants";
 
-const gallery: Restaurant_Collection = new Restaurant_Collection(BACKEND_ROOT_URL);
+/* const gallery: Restaurant_Collection = new Restaurant_Collection(BACKEND_ROOT_URL);
 
 gallery
   .getRestuarants()
@@ -14,34 +14,64 @@ gallery
   })
   .catch((error) => {
     alert(error);
-  });
+  }); */
+  
 
-function renderRestaurant(restaurant_from_db) {
+let restaurant_data = [];
+fetch("http://localhost:3001/api/restaurants")
+  .then((response) => response.json())
+  .then((json) => {
+    console.log("full json", json);
+    restaurant_data = json;
+
+    console.log("restaurant_data", restaurant_data.length);
+
+    //show all restaurants
+    if (restaurant_data) {
+      // excute only if restaurant_data is not undefined
+      for (let i = 0; i < restaurant_data.length; i++) {
+        console.log("-------name",restaurant_data[i].restaurant_name);
+      }
+      renderRestaurant(restaurant_data[0]);
+
+    }
+  })
+  .catch((error) => console.log(error));
+
+
+const parentID = "restaurant-info";
+
+function renderRestaurant( restaurant_data: any) {
+  if (!restaurant_data) return; // checking null
+
   // create div element with class "col-md-8 mx-auto text-center"
-  const divElement = document.createElement('div');
-  divElement.className = 'col-md-8 mx-auto text-center';
+  const divElement = document.createElement("div");
+  divElement.className = "col-md-8 mx-auto text-center";
 
   // create h6 element with class "text-success" and text content "More about restaurant"
-  const h6Element = document.createElement('h6');
-  h6Element.className = 'text-success';
-  h6Element.textContent = 'More about restaurant';
+  const h6Element = document.createElement("h6");
+  h6Element.className = "text-success";
+  h6Element.textContent = "More about restaurant";
 
   // create h1 element with text content "Name / Rocket Burger"
-  const h1Element = document.createElement('h1');
-  h1Element.textContent = 'Name / Rocket Burger';
+  const h1Element = document.createElement("h1");
+  h1Element.textContent = `${restaurant_data.restaurant_name}`;
 
   // create p element with text content "phone_number: string street_address: string city: string zip_code: string"
-  const pElement = document.createElement('p');
-  pElement.textContent = `phone_number: ${restaurant_from_db.phone_number} street_address: ${restaurant_from_db.street_address} city: ${restaurant_from_db.city} zip_code: ${restaurant_from_db.zip_code}`;
+  const pElement = document.createElement("p");
+  pElement.textContent = `${restaurant_data.street_address}  ,${restaurant_data.city}  , ${restaurant_data.zip_code} / ${restaurant_data.phone_number}`;
 
   // append child elements to div element
   divElement.appendChild(h6Element);
   divElement.appendChild(h1Element);
   divElement.appendChild(pElement);
 
-  // // get parent element and append div element to it
-  // const parentElement = document.getElementById(parentID);
-  // if (parentElement) {
-  //   parentElement.appendChild(divElement);
-  // }
+  // get parent element and append div element to it
+  const parentElement = document.getElementById(parentID);
+  if (parentElement) {
+    parentElement.appendChild(divElement);
+  }
 }
+
+//review  section
+
