a743047886ec10d614a53aae51a9acf50ea658cc
Tomoko Takami <t2tato01@students.oamk.fi>
2023-04-28 11:57:50 +0300

    add DOM  of full-reviews

--- ts/render_restaurant_view.ts
+++ ts/render_restaurant_view.ts
@@ -73,5 +73,113 @@ function renderRestaurant( restaurant_data: any) {
   }
 }
 
+
+
 //review  section
 
+
+let reviews_data = [];
+fetch("http://localhost:3001/api/reviews")
+  .then((response) => response.json())
+  .then((json) => {
+    console.log("full json", json);
+    reviews_data = json;
+
+    console.log("reviews_data", reviews_data.length);
+
+    //show all restaurants
+    if (reviews_data) {
+      // excute only if restaurant_data is not undefined
+      for (let i = 0; i < restaurant_data.length; i++) {
+        console.log("-------title",reviews_data[i].review_title);
+      }
+      renderReview(reviews_data[0]);
+      renderReview(reviews_data[9]);
+      renderReview(reviews_data[2]);
+      renderReview(reviews_data[3]);
+      renderReview(reviews_data[4]);
+
+    }
+  })
+  .catch((error) => console.log(error));
+
+/* 
+  <div class="col-lg-4 col-sm-6">
+            <div class="recommendation">
+              <img src="../server/public/images/breads.jpg" alt="food">
+              <div class="beforeoverlay">
+                <h4 class="text-white">stars: number / title / date</h4>
+               </div>
+               <div class="overlay">
+                <h4 class="text-white">stars: number / title / date</h4>
+                <p class="text-white">
+                  id: number
+                  title: string
+                  body: string
+                  stars: number
+                  restaurant_id: number
+                  user_id: number
+                  date: string</p>
+              </div>
+            </div> */
+
+//dom 
+const parentID2 = "full-reviews";
+
+function renderReview( reviews_data: any) {
+  if (!reviews_data) return; // checking null
+
+  // create div element with class "col-lg-4 col-sm-6"
+  const divElement = document.createElement("div");
+  divElement.className = "col-lg-4 col-sm-6";
+
+  // create div element with class "recommendation"
+  const divElement2 = document.createElement("div");
+  divElement2.className = "recommendation";
+
+  // !create img element with src "../server/public/images/breads.jpg" and alt "food"
+  const imgElement = document.createElement("img");
+  imgElement.src = "../server/public/images/breads.jpg";
+  imgElement.alt = "food";
+
+  // create div element with class "beforeoverlay"
+  const divElement3 = document.createElement("div");
+  divElement3.className = "beforeoverlay";
+
+  // create h4 element with class "text-white" and text content "stars: number / title / date"
+  const h4Element = document.createElement("h4");
+  h4Element.className = "text-white";
+  h4Element.textContent = `${reviews_data.stars} / ${reviews_data.review_title} `;
+
+  // create div element with class "overlay"
+  const divElement4 = document.createElement("div");
+  divElement4.className = "overlay";
+
+  // create h4 element with class "text-white" and text content "stars: number / title / date"
+  const h4Element2 = document.createElement("h4");
+  h4Element2.className = "text-white";
+  h4Element2.textContent = `${reviews_data.stars} / ${reviews_data.review_title} `; 
+
+  // create p element with class "text-white" and text content "id: number title: string body: string stars: number restaurant_id: number user_id: number date: string"
+  const pElement = document.createElement("p");
+  pElement.className = "text-white";
+  pElement.textContent = `${reviews_data.review_body} date: ${reviews_data.created_at}`;
+
+  // append child elements to div element
+  divElement.appendChild(divElement2);
+
+  divElement2.appendChild(imgElement);
+  divElement2.appendChild(divElement3);
+  divElement2.appendChild(divElement4);
+
+  divElement3.appendChild(h4Element);
+  divElement4.appendChild(h4Element2);
+  divElement4.appendChild(pElement);
+
+  // get parent element and append div element to it
+  const parentElement = document.getElementById(parentID2);
+  if (parentElement) {
+    parentElement.appendChild(divElement);
+  }
+
+}
