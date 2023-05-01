06350c5e981303376eddf38d458b4095752a8f9b
Tomoko Takami <t2tato01@students.oamk.fi>
2023-05-01 23:43:09 +0300

    add page for showing all restaurant

--- ts/all-reviews-created_at.ts
+++ ts/all-reviews-created_at.ts
@@ -66,7 +66,7 @@ function renderReviewCard2(reviews_data_allreviews: any) {
 
   // create div element with class "col-lg-4 col-sm-6"
   const divElement = document.createElement("div");
-  divElement.className = "col-lg-4 col-sm-6";
+  divElement.className = "col-lg-3 col-sm-4";
 
   // create div element with class "recommendation"
   const divElement2 = document.createElement("div");
